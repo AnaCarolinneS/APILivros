09cd71152a230949b334660f4e07b15a6529ccc5
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-07-10 13:50:49 -0300

    Finalização do curso
    Finalização do curso

--- src/livros.controller.ts
+++ src/livros.controller.ts
@@ -7,30 +7,30 @@ export class LivrosController {
     constructor(private livrosService: LivrosService) { }
 
     @Get()
-    obterTodos(): Livro[] {
+    async obterTodos(): Promise<Livro[]> {
         return this.livrosService.obterTodos();
     }
 
     @Get(':id')
-    obterUm(@Param() params): Livro {
+    async obterUm(@Param() params): Promise<Livro> {
         return this.livrosService.obterUm(params.id);
     }
 
     @Post()
-    criarLivro(@Body() livro: Livro) {
+    async criarLivro(@Body() livro: Livro) {
         livro.id = 100;
         this.livrosService.criarLivro(livro);
         return 'Livro adicionado!';
     }
 
     @Put()
-    alterarLivro(@Body() livro: Livro): Livro {
+    async alterarLivro(@Body() livro: Livro): Promise<[number]> {
         console.log(livro);
         return this.livrosService.alterarLivro(livro);
     }
 
     @Delete(':id')
-    excluirLivro(@Param() params) {
+    async excluirLivro(@Param() params) {
         this.livrosService.apagarLivro(params.id);
         return 'Livro apagado!';
     }
--- src/livros.service.ts
+++ src/livros.service.ts
@@ -1,33 +1,38 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
 import { Livro } from "./livro.model";
 
 @Injectable() //injetando classes em outras - no caso, injetando no controlador
 //injeção de dependencia é um padrao de projeto que ajuda a manter um baixo nivel de acoplamento entre classes.
 //sao definidas pelo framework.
 export class LivrosService {
-    livros: Livro[] = [
-        /* new Livro('Livro001', 'Livro Javascript', 30.00),
-        new Livro('Livro002', 'Livro MongoDB', 60.00),
-        new Livro('Livro003', 'Livro Java', 99.00) */
-    ];
+    constructor(
+        @InjectModel(Livro)
+        private livroModel: typeof Livro
+    ) { }
 
-    obterTodos(): Livro[] {
-        return this.livros;
+    async obterTodos(): Promise<Livro[]> {
+        return this.livroModel.findAll();
     }
 
-    obterUm(id: number): Livro {
-        return this.livros[0];
+    async obterUm(id: number): Promise<Livro> {
+        return this.livroModel.findByPk(id);
     }
 
-    criarLivro(livro: Livro) {
-        this.livros.push(livro);
+    async criarLivro(livro: Livro) {
+        this.livroModel.create(livro);
     }
 
-    alterarLivro(livro: Livro) : Livro {
-        return livro;
-    }
+    async alterarLivro(livro: Livro) : Promise<[number]> {
+        return this.livroModel.update(livro, {
+            where: {
+                id: livro.id,
+            }
+        });
+     }
 
-    apagarLivro(id: number) {
-        this.livros.pop();
+    async apagarLivro(id: number) {
+        const livro: Livro = await this.obterUm(id);
+        livro.destroy;
     }
 }
